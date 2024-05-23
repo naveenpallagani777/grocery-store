9d9eedf817f210d993c2d18da15d0fb1883ec4cc
naveenpnk <naveenpallagani777@gmail.com>
2024-05-23 12:00:30 +0530

    changes

--- Controllers/controller.js
+++ Controllers/controller.js
@@ -1,5 +1,16 @@
-const reader = require("../Reading_html/reader")
+const reader = require("../Reading_html/reader");
+const {Farmer, Customer} = require("../modelSchema/modelschema");
+const mongoose = require("mongoose");
 
 exports.homepage = (req,res) => {
+    Customer.find()
+.then(data => {
+    console.log(data);
+})
+.catch(err => {
+    console.log(err);
+})
     res.status(200).send(reader.homepage);
+
 }
+
--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 // const reader = require('./Reading_html/reader');
 const route = require("./Routers/routers");
-app.use(express.json());
+
+
+
 app.use(express.static('./public'));
 
 app.use('/home',route);
+
+mongoose.connect("mongodb+srv://admin:B5Kp27Up0AMo1vK8@cluster0.mk3wexz.mongodb.net/Grocery_Store?retryWrites=true&w=majority&appName=Cluster0",
+).then((conn) => {
+    // console.log(conn);
+    console.log('DB connect successful');
+}).catch((error) => {
+    console.log(error);
+})
+
+
+
 
 
 app.listen("3000",()=>{
